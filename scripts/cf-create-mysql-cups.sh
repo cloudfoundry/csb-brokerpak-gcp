a9912cad260b996738ee1b9ea09fd602f50da5fa
Ernie Billing <ebilling@pivotal.io>
2021-02-24 18:41:33 -0800

    fixed broker release download for new csb repo, added cups helper script

---
+++ scripts/cf-create-mysql-cups.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+
+if [ $# -lt 4 ]; then
+    echo "Usage: ${0} <hostname> <username> <password> <db_name>"
+    exit 1
+fi
+
+HOSTNAME=${1}; shift
+USERNAME=${1}; shift
+PASSWORD=${1}; shift
+DB_NAME=${1}; shift
+PORT=3306
+
+cf cups csb-sql -p "{\"hostname\":"${HOSTNAME}", \
+                     \"username\":"${USERNAME}", \
+                     \"password\":"${PASSWORD}", \
+                     \"name\":"${DB_NAME}", \
+                     \"port\":${PORT}, \
+                     \"uri\":"mysql://${USERNAME}:${PASSWORD}@${HOSTNAME}:${PORT}/${DB_NAME}"}" \
+                -t mysql
