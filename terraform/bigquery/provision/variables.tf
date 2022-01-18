ee1dab3e8f71be16d358fa5615b460eb0c5d4aba
George Blue <gblue@pivotal.io>
2022-01-18 17:12:43 +0000

    refactor: separate out BigQuery terraform
    By separating out the terraform into separate files it makes it easier
    to read, and also drives the "initializeFsFlat()" code path in the
    broker which is more amenable to future HCL updates require when
    updating the terraform version
    
    [#180858769](https://www.pivotaltracker.com/story/show/180858769)

---
+++ terraform/bigquery/provision/variables.tf
@@ -0,0 +1,5 @@
+variable "credentials" { type = string }
+variable "project" { type = string }
+variable "labels" { type = map(any) }
+variable "region" { type = string }
+variable "instance_name" { type = string }
