e8ec046c81e53e89918786b492997091f41d56c0
Marcela Campo <mcampo@pivotal.io>
2022-02-07 12:21:46 +0000

    tests: redis upgrade test
    [#180082688](https://www.pivotaltracker.com/story/show/180082688)

---
+++ acceptance-tests/upgrade/upgrade_suite_test.go
@@ -0,0 +1,30 @@
+package upgrade_test
+
+import (
+	"acceptancetests/helpers/environment"
+	"flag"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var (
+	developmentBuildDir string
+	releasedBuildDir    string
+	metadata            environment.GCPMetadata
+)
+
+func init() {
+	flag.StringVar(&releasedBuildDir, "releasedBuildDir", "../../../azure-released", "location of released version of built broker and brokerpak")
+	flag.StringVar(&developmentBuildDir, "developmentBuildDir", "../../dev-release", "location of development version of built broker and brokerpak")
+}
+
+var _ = BeforeSuite(func() {
+	metadata = environment.ReadGCPMetadata()
+})
+
+func TestUpgrade(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Upgrade Suite")
+}
