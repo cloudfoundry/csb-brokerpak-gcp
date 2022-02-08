42fff1cc71a80b3d10ffd4429820c5a24e80c851
Marcela Campo <mcampo@pivotal.io>
2022-02-08 12:00:00 +0000

    test: change plan for redis upgrade test
    [#180082688](https://www.pivotaltracker.com/story/show/180082688)

--- acceptance-tests/upgrade/update_and_upgrade_redis_test.go
+++ acceptance-tests/upgrade/update_and_upgrade_redis_test.go
@@ -67,7 +67,7 @@ var _ = Describe("UpgradeRedisTest", Label("redis"), func() {
 			Expect(appTwo.GET(key1)).To(Equal(value1))
 
 			By("updating the instance plan")
-			serviceInstance.Update("-p", "medium")
+			serviceInstance.Update("-p", "ha")
 
 			By("checking it still works")
 			key3 := random.Hexadecimal()
