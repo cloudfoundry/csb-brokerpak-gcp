dd20e6348dc332260c572d27965327b351dcd3f0
Felisia Martini <fmartini@pivotal.io>
2022-03-01 12:37:15 +0000

    test: Tidy up mysql tests

--- integration-tests/mysql_test.go
+++ integration-tests/mysql_test.go
@@ -14,45 +14,56 @@ var _ = Describe("Mysql", func() {
 	AfterEach(func() {
 		Expect(mockTerraform.Reset()).NotTo(HaveOccurred())
 	})
-	It("publish mysql in the catalog", func() {
+
+	It("should publish mysql in the catalog", func() {
 		catalog, err := broker.Catalog()
+
 		Expect(err).NotTo(HaveOccurred())
+
 		service := testframework.FindService(catalog, "csb-google-mysql")
 		Expect(service.Plans).To(HaveLen(3))
 		Expect(service.Tags).To(ContainElement("beta"))
 		Expect(service.Metadata.ImageUrl).NotTo(BeNil())
 	})
 
-	It("should provision small plan", func() {
-		broker.Provision("csb-google-mysql", "small", nil)
+	Describe("provisioning", func() {
+		It("should provision small plan", func() {
+			broker.Provision("csb-google-mysql", "small", nil)
 
-		invocations, err := mockTerraform.ApplyInvocations()
-		Expect(err).NotTo(HaveOccurred())
-		Expect(invocations).To(HaveLen(1))
+			invocations, err := mockTerraform.ApplyInvocations()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(invocations).To(HaveLen(1))
 
-		contents, err := invocations[0].TFVarsContents()
-		Expect(err).NotTo(HaveOccurred())
-		Expect(replaceGUIDs(contents)).To(MatchJSON(getResultContents("mysql-result")))
+			contents, err := invocations[0].TFVarsContents()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(replaceGUIDs(contents)).To(MatchJSON(getResultContents("mysql-result")))
+		})
+
+		It("should allow setting of database name", func() {
+			broker.Provision("csb-google-mysql", "small", map[string]interface{}{"db_name": "foobar"})
+
+			Expect(mockTerraform.FirstTerraformInvocationVars()).To(HaveKeyWithValue("db_name", "foobar"))
+		})
+
+		It("should not allow changing of plan defined properties", func() {
+			_, err := broker.Provision("csb-google-mysql", "small", map[string]interface{}{"cores": 5})
+
+			Expect(err).To(MatchError(ContainSubstring("plan defined properties cannot be changed: cores")))
+		})
 	})
 
-	It("user should be able to update database name", func() {
-		broker.Provision("csb-google-mysql", "small", map[string]interface{}{"db_name": "foobar"})
-		Expect(mockTerraform.FirstTerraformInvocationVars()).To(HaveKeyWithValue("db_name", "foobar"))
-	})
+	Describe("property validations", func() {
+		It("should validate region", func() {
+			_, err := broker.Provision("csb-google-mysql", "small", map[string]interface{}{"region": "invalid-region"})
 
-	It("user should not be allowed to change mysql cores", func() {
-		_, err := broker.Provision("csb-google-mysql", "small", map[string]interface{}{"cores": 5})
-		Expect(err).To(MatchError(ContainSubstring("plan defined properties cannot be changed: cores")))
-	})
+			Expect(err).To(MatchError(ContainSubstring("region must be one of the following:")))
+		})
 
-	It("should validate region", func() {
-		_, err := broker.Provision("csb-google-mysql", "small", map[string]interface{}{"region": "invalid-region"})
-		Expect(err).To(MatchError(ContainSubstring("region must be one of the following:")))
-	})
+		It("should validate instance name length", func() {
+			_, err := broker.Provision("csb-google-mysql", "small", map[string]interface{}{"instance_name": "2smol"})
 
-	It("should validate instance name length", func() {
-		_, err := broker.Provision("csb-google-mysql", "small", map[string]interface{}{"instance_name": "2smol"})
-		Expect(err).To(MatchError(ContainSubstring("instance_name: String length must be greater than or equal to 6")))
+			Expect(err).To(MatchError(ContainSubstring("instance_name: String length must be greater than or equal to 6")))
+		})
 	})
 })
 
