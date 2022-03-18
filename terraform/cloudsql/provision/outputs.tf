abe924459913db016c9640a268ee4d3fafef8ddb
Jatin Naik <jatinn@vmware.com>
2022-03-18 10:48:53 +0000

    fix: configure provider specific ports from the brokerpak files (#253)
    [#181059661]

--- terraform/cloudsql/provision/outputs.tf
+++ terraform/cloudsql/provision/outputs.tf
@@ -1,7 +1,7 @@
 output "name" { value = google_sql_database.database.name }
 output "hostname" { value = google_sql_database_instance.instance.first_ip_address }
 
-output "port" { value = (var.database_version == "POSTGRES_11" ? 5432 : 3306) }
+output "port" { value = var.db_port }
 output "username" { value = google_sql_user.admin_user.name }
 output "password" {
   sensitive = true
