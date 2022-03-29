a406d9045fc7d1835082f40d43a9441136e7b250
James Norman <normanja@vmware.com>
2022-03-29 15:20:17 +0100

    amend typo

--- terraform/cloudsql/mysql/bind/provider.tf
+++ terraform/cloudsql/mysql/bind/provider.tf
@@ -1,5 +1,5 @@
 provider "mysql" {
-  endpoint = format("%s:%d", var.mysql_hostname, locals.port)
+  endpoint = format("%s:%d", var.mysql_hostname, local.port)
   username = var.admin_username
   password = var.admin_password
 }
