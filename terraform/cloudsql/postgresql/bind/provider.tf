f5cff4b75aba3f14d4550574222fa963b82c65e7
Jatin Naik <jatinn@vmware.com>
2022-04-07 16:51:05 +0530

    feat: enabling tls for postgres by default (#274)
    [#181059797](https://www.pivotaltracker.com/story/show/181059797)
    
    Co-authored-by: Felisia Martini <fmartini@pivotal.io>

--- terraform/cloudsql/postgresql/bind/provider.tf
+++ terraform/cloudsql/postgresql/bind/provider.tf
@@ -1,9 +1,16 @@
 provider "postgresql" {
-  host      = var.hostname
-  port      = local.port
-  username  = var.admin_username
-  password  = var.admin_password
-  superuser = false
-  database  = var.db_name
-  sslmode   = var.use_tls ? "require" : "disable"
+  host        = var.hostname
+  port        = local.port
+  username    = var.admin_username
+  password    = var.admin_password
+  superuser   = false
+  database    = var.db_name
+  sslmode     = var.use_tls ? "verify-ca" : "disable"
+  clientcert {
+    cert = "${path.module}/sslcert.pem"
+    key  = "${path.module}/sslkey.pem"
+  }
+  sslrootcert = "${path.module}/sslrootcert.pem"
 }
+
+provider "local" {}
--- terraform/cloudsql/postgresql/provision/outputs.tf
+++ terraform/cloudsql/postgresql/provision/outputs.tf
@@ -6,4 +6,11 @@ output "password" {
   sensitive = true
   value     = postgresql_role.createrole_user.password
 }
-output "use_tls" { value = false }
+output "use_tls" { value = var.use_tls }
+
+output "sslcert" { value = google_sql_ssl_cert.client_cert.cert }
+output "sslkey" {
+    value = google_sql_ssl_cert.client_cert.private_key
+    sensitive   = true
+}
+output "sslrootcert" { value = google_sql_database_instance.instance.server_ca_cert.0.cert }
