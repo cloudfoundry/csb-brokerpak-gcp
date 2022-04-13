974ee026ed1fb53c52e35f7ddb63665df6cd3771
George Blue <blgm@users.noreply.github.com>
2022-04-13 08:08:47 +0100

    test: PostgreSQL app should be able to use TLS (#295)
    This restores functionality from commit
    faa973c9fd4793d3933f2dae02186ef4ff19b1dc which had to be reverted. The
    TLS functionality is not used yet. The plan is to enable that in a
    future commit.
    
    [#181059797](https://www.pivotaltracker.com/story/show/181059797)
    
    Co-authored-by: James Norman <normanja@vmware.com>

--- acceptance-tests/apps/postgresqlapp/main.go
+++ acceptance-tests/apps/postgresqlapp/main.go
@@ -13,14 +13,14 @@ func main() {
 	log.Println("Starting.")
 
 	log.Println("Reading credentials.")
-	creds, err := credentials.Read()
+	uri, err := credentials.Read()
 	if err != nil {
 		panic(err)
 	}
 
 	port := port()
 	log.Printf("Listening on port: %s", port)
-	http.Handle("/", app.App(creds))
+	http.Handle("/", app.App(uri))
 	http.ListenAndServe(port, nil)
 }
 
