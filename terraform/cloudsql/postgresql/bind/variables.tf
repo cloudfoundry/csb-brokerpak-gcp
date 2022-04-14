74926d98dd35b9e855639e9154bba611a62865be
George Blue <blgm@users.noreply.github.com>
2022-04-14 14:37:42 +0100

    feat(PostgreSQL): connections should require TLS (#299)
    This restores the functionality from f5cff4b which was previously reverted.
    
    [#181059797](https://www.pivotaltracker.com/story/show/181059797)

--- terraform/cloudsql/postgresql/bind/variables.tf
+++ terraform/cloudsql/postgresql/bind/variables.tf
@@ -2,7 +2,10 @@ variable "db_name" { type = string }
 variable "hostname" { type = string }
 variable "admin_username" { type = string }
 variable "admin_password" { type = string }
-variable "use_tls" { type = bool }
+#variable "use_tls" { type = bool }
+variable "sslcert" { type = string }
+variable "sslkey" { type = string }
+variable "sslrootcert" { type = string }
 
 locals {
   port = 5432
