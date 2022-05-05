44c8c5251d8ae753eeee92287d85bba75732fba6
Jatin Naik <jatinn@vmware.com>
2022-05-05 12:10:42 +0100

    feat: using the custom provider for bindings
    [#181898458]
    
    Co-authored-by: Felisia Martini <fmartini@pivotal.io>

--- terraform/cloudsql/postgresql/provision/provider.tf
+++ terraform/cloudsql/postgresql/provision/provider.tf
@@ -2,18 +2,3 @@ provider "google" {
   credentials = var.credentials
   project     = var.project
 }
-
-provider "postgresql" {
-  host        = google_sql_database_instance.instance.first_ip_address
-  port        = 5432
-  username    = google_sql_user.admin_user.name
-  password    = google_sql_user.admin_user.password
-  superuser   = false
-  database    = google_sql_database.database.name
-  sslmode     = "verify-ca"
-  sslrootcert = local_file.sslrootcert.filename
-  clientcert {
-    cert = local_file.sslcert.filename
-    key  = local_sensitive_file.sslkey.filename
-  }
-}
