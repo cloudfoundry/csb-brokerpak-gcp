927f7e1a6285243c55e4974eac72621abe7b30dc
Jatin Naik <jatinn@vmware.com>
2022-05-05 14:18:52 +0100

    feat: using the custom provider for bindings (#335)
    [#181898458]
    
    Co-authored-by: Felisia Martini <fmartini@pivotal.io>
    
    Co-authored-by: Felisia Martini <fmartini@pivotal.io>

--- terraform/cloudsql/postgresql/bind/versions.tf
+++ terraform/cloudsql/postgresql/bind/versions.tf
@@ -1,18 +1,13 @@
 terraform {
   required_providers {
-    postgresql = {
-      source  = "cyrilgdn/postgresql"
-      version = ">=1.15.0"
-    }
-
     random = {
       source  = "hashicorp/random"
       version = ">=3.1.0"
     }
 
-    local = {
-      source  = "hashicorp/local"
-      version = ">=2.2.2"
+    csbpg = {
+      source  = "cloud-service-broker/csbpg"
+      version = "1.0.0"
     }
   }
 }
--- terraform/cloudsql/postgresql/provision/outputs.tf
+++ terraform/cloudsql/postgresql/provision/outputs.tf
@@ -1,10 +1,10 @@
 output "name" { value = google_sql_database.database.name }
 output "hostname" { value = google_sql_database_instance.instance.first_ip_address }
 
-output "username" { value = postgresql_role.createrole_user.name }
+output "username" { value = google_sql_user.admin_user.name }
 output "password" {
   sensitive = true
-  value     = postgresql_role.createrole_user.password
+  value     = google_sql_user.admin_user.password
 }
 output "use_tls" { value = true }
 
