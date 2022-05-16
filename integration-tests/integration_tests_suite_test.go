f8b957aac21600bd5f49fe322938617ca9a3efa9
Andrea Zucchini <zandrea@vmware.com>
2022-05-16 12:53:52 +0100

    test: add new custom MySQL plan
    We want to test the user inputs in properties already defined in the
    default plans, due to it is not possible to provide these properties
    because the system does not allow us to override them, we create a new
    custom plan to add a new test node.
    
    [#181404871](https://www.pivotaltracker.com/story/show/181404871)

--- integration-tests/integration_tests_suite_test.go
+++ integration-tests/integration_tests_suite_test.go
@@ -27,19 +27,23 @@ const (
 var _ = BeforeSuite(func() {
 	var err error
 	mockTerraform, err = testframework.NewTerraformMock()
-	Expect(err).NotTo(HaveOccurred())
+	Expect(err).ShouldNot(HaveOccurred())
 
 	broker, err = testframework.BuildTestInstance(testframework.PathToBrokerPack(), mockTerraform, GinkgoWriter)
-	Expect(err).NotTo(HaveOccurred())
+	Expect(err).ShouldNot(HaveOccurred())
 
 	postgresPlansJSON, err := json.Marshal(postgresPlans)
-	Expect(err).NotTo(HaveOccurred())
+	Expect(err).ShouldNot(HaveOccurred())
+
+	mySQLPlansJSON, err := json.Marshal([]map[string]interface{}{customMySQLPlan})
+	Expect(err).ShouldNot(HaveOccurred())
 
 	Expect(broker.Start(GinkgoWriter, []string{
 		"GSB_COMPATIBILITY_ENABLE_BETA_SERVICES=true",
 		"GOOGLE_CREDENTIALS=" + BrokerGCPCreds,
 		"GOOGLE_PROJECT=" + BrokerGCPProject,
 		`GSB_SERVICE_CSB_GOOGLE_POSTGRES_PLANS=` + string(postgresPlansJSON),
+		"GSB_SERVICE_CSB_GOOGLE_MYSQL_PLANS=" + string(mySQLPlansJSON),
 		"CSB_LISTENER_HOST=localhost", // prevents permissions popup
 	})).To(Succeed())
 })
