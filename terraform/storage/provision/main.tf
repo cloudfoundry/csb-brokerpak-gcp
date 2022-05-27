c7323e50f1ccddd48b5d43844afbaea985279d26
George Blue <gblue@pivotal.io>
2022-05-27 13:55:04 +0100

    fix: add lifecycle.prevent_destroy to data services
    [#182284156](https://www.pivotaltracker.com/story/show/182284156)

--- terraform/storage/provision/main.tf
+++ terraform/storage/provision/main.tf
@@ -3,4 +3,8 @@ resource "google_storage_bucket" "bucket" {
   location      = var.region
   storage_class = var.storage_class
   labels        = var.labels
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
