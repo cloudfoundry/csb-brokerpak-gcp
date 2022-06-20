f4b56cc4da00dcd6aad41828a1fbc02270d77eb7
Andrea Zucchini <zandrea@vmware.com>
2022-06-20 17:04:46 +0100

    chore: set dot import white list
    [#182299591](https://www.pivotaltracker.com/story/show/182299591)

---
+++ integration-tests/storage_test.go
@@ -0,0 +1,50 @@
+package integration_test
+
+import (
+	testframework "github.com/cloudfoundry/cloud-service-broker/brokerpaktestframework"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("BigQuery", func() {
+	const serviceName = "csb-google-bigquery"
+
+	BeforeEach(func() {
+		Expect(mockTerraform.SetTFState([]testframework.TFStateValue{})).To(Succeed())
+	})
+
+	AfterEach(func() {
+		Expect(mockTerraform.Reset()).To(Succeed())
+	})
+
+	Describe("provisioning", func() {
+		It("should check region constraints", func() {
+			_, err := broker.Provision(serviceName, "standard", map[string]any{"region": "-Asia-northeast1"})
+			Expect(err).To(MatchError(ContainSubstring("region: Does not match pattern '^[a-z][a-z0-9-]+$'")))
+		})
+	})
+
+	Describe("updating instance", func() {
+		var instanceID string
+
+		BeforeEach(func() {
+			var err error
+			instanceID, err = broker.Provision(serviceName, "standard", nil)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(mockTerraform.Reset()).To(Succeed())
+		})
+
+		It("should allow updating region because it is not flagged as `prohibit_update` and not specified in the plan", func() {
+			err := broker.Update(instanceID, serviceName, "standard", map[string]any{"region": "asia-southeast1"})
+
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should allow updating region because it is not flagged as `prohibit_update` and not specified in the plan", func() {
+			err := broker.Update(instanceID, serviceName, "standard", map[string]any{"region": "asia-southeast1"})
+
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+})
