5cc9edc9d0a2a29c34c116766d1fda7ffa40f154
George Blue <blgm@users.noreply.github.com>
2022-07-22 08:55:12 +0100

    test: speed up broker update (#429)
    [#182771200](https://www.pivotaltracker.com/story/show/182771200)

--- acceptance-tests/helpers/brokers/update.go
+++ acceptance-tests/helpers/brokers/update.go
@@ -6,21 +6,24 @@ import (
 )
 
 func (b *Broker) UpdateBroker(dir string) {
+	WithEnv(b.latestEnv()...)(b)
+
 	b.app.Push(
 		apps.WithName(b.Name),
 		apps.WithDir(dir),
+		apps.WithStartedState(),
+		apps.WithManifest(newManifest(
+			withName(b.Name),
+			withEnv(b.env()...),
+		)),
 	)
-
-	WithEnv(b.latestEnv()...)(b)
-	b.app.SetEnv(b.env()...)
-	b.app.Restart()
 
 	cf.Run("update-service-broker", b.Name, b.username, b.password, b.app.URL)
 }
 
 func (b *Broker) UpdateEnv(env ...apps.EnvVar) {
 	WithEnv(env...)(b)
-	b.app.SetEnv(b.env()...)
+	b.app.SetEnv(env...)
 	b.app.Restart()
 
 	cf.Run("update-service-broker", b.Name, b.username, b.password, b.app.URL)
