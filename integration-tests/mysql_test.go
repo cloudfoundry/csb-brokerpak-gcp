3ee3fd82677d46ab6b2525fa9ea0f75cde311d2e
Andrea Zucchini <zandrea@vmware.com>
2022-12-19 13:05:34 +0000

    feat: remove authorized_network parameter (#586)
    * feat: remove authorized_network parameter
    
    [#183975388](https://www.pivotaltracker.com/story/show/183975388)

--- integration-tests/mysql_test.go
+++ integration-tests/mysql_test.go
@@ -65,7 +65,6 @@ var _ = Describe("Mysql", Label("MySQL"), func() {
 			Expect(mockTerraform.FirstTerraformInvocationVars()).To(
 				SatisfyAll(
 					HaveKeyWithValue("db_name", "csb-db"),
-					HaveKeyWithValue("authorized_network", "default"),
 					HaveKeyWithValue("authorized_network_id", ""),
 					HaveKeyWithValue("credentials", "broker-gcp-creds"),
 					HaveKeyWithValue("mysql_version", "MYSQL_5_7"),
@@ -94,7 +93,6 @@ var _ = Describe("Mysql", Label("MySQL"), func() {
 				"instance_name":                          "fakeinstancename",
 				"db_name":                                "fake-db_name",
 				"region":                                 "asia-northeast1",
-				"authorized_network":                     "fake-authorized_network",
 				"authorized_network_id":                  "fake-authorized_network_id",
 				"tier":                                   "fake-tier",
 				"disk_autoresize":                        true,
@@ -114,7 +112,6 @@ var _ = Describe("Mysql", Label("MySQL"), func() {
 					HaveKeyWithValue("instance_name", "fakeinstancename"),
 					HaveKeyWithValue("db_name", "fake-db_name"),
 					HaveKeyWithValue("region", "asia-northeast1"),
-					HaveKeyWithValue("authorized_network", "fake-authorized_network"),
 					HaveKeyWithValue("authorized_network_id", "fake-authorized_network_id"),
 					HaveKeyWithValue("tier", "fake-tier"),
 					HaveKeyWithValue("disk_autoresize", true),
@@ -243,7 +240,6 @@ var _ = Describe("Mysql", Label("MySQL"), func() {
 			Entry("update instance_name", map[string]any{"instance_name": "another-instance-name"}),
 			Entry("update db_name", map[string]any{"db_name": "another-db-name"}),
 			Entry("update region", map[string]any{"region": "australia-southeast1"}),
-			Entry("update authorized_network", map[string]any{"authorized_network": "another-authorized-network"}),
 			Entry("update authorized_network_id", map[string]any{"authorized_network_id": "another-authorized-network_id"}),
 		)
 
