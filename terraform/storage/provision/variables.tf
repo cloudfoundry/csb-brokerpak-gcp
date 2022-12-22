e731cc68aa4529446af75ec183eb0256df83aa02
Andrea Zucchini <azga84@gmail.com>
2022-12-22 23:00:54 +0000

    chore: Remove acl plan parameter (#593)
    The parameter is not being used.
    
    [#184053404](https://www.pivotaltracker.com/story/show/184053404)

--- terraform/storage/provision/variables.tf
+++ terraform/storage/provision/variables.tf
@@ -2,6 +2,5 @@ variable "name" { type = string }
 variable "region" { type = string }
 variable "storage_class" { type = string }
 variable "labels" { type = map(any) }
-variable "acl" { type = string }
 variable "credentials" { type = string }
 variable "project" { type = string }
