c10bdfa4f0e11218e6633ac82100b8895768fcf2
Konstantin Semenov <jhvhs@users.noreply.github.com>
2022-12-22 15:26:09 +0000

    chore: [GCP MySQL & PostgreSQL] Fix public IP binding creation (#591)
    * chore: [GCP MySQL & PostgreSQL] Fix public IP binding creation
    
    [#184083158](https://www.pivotaltracker.com/story/show/184083158)
    
    * chore: format code
    
    [#183876947](https://www.pivotaltracker.com/story/show/183876947)
    
    * fix: follow linter rules
    
    [#184051243](https://www.pivotaltracker.com/story/show/184051243)
    
    * fix: bind test expects the private ip parameter
    
    [#183876947](https://www.pivotaltracker.com/story/show/183876947)
    
    Co-authored-by: Andrea Zucchini <zandrea@vmware.com>

--- terraform/cloudsql/mysql/bind/variables.tf
+++ terraform/cloudsql/mysql/bind/variables.tf
@@ -1,7 +1,11 @@
 variable "mysql_db_name" { type = string }
 variable "mysql_hostname" { type = string }
+variable "private_ip" { type = string }
 variable "admin_username" { type = string }
-variable "admin_password" { type = string }
+variable "admin_password" {
+  sensitive = true
+  type      = string
+}
 variable "use_tls" { type = bool }
 
 locals {
