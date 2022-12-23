d5bdf252a0fa4d283ed0f91f7ec4fd5dcca728c9
Andrea Zucchini <azga84@gmail.com>
2022-12-23 17:12:17 +0000

    chore: binding result using snake case format (#595)
    [#184053361](https://www.pivotaltracker.com/story/show/184053361)

--- terraform/storage/bind/outputs.tf
+++ terraform/storage/bind/outputs.tf
@@ -1,12 +1,12 @@
-output "Name" { value = google_service_account.account.name }
-output "Email" { value = google_service_account.account.email }
-output "UniqueId" { value = google_service_account.account.unique_id }
-output "PrivateKeyData" {
+output "name" { value = google_service_account.account.name }
+output "email" { value = google_service_account.account.email }
+output "unique_id" { value = google_service_account.account.unique_id }
+output "private_key_data" {
   sensitive = true
   value     = google_service_account_key.key.private_key
 }
-output "ProjectId" { value = google_service_account.account.project }
-output "Credentials" {
+output "project_id" { value = google_service_account.account.project }
+output "credentials" {
   sensitive = true
   value     = base64decode(google_service_account_key.key.private_key)
 }
