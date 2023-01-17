927e1fbc794712596badf3245b66cc4ab736abd0
Andrea Zucchini <zandrea@vmware.com>
2023-01-17 16:56:35 +0000

    feat: [Cloud Storage] expose autoclass property
    [#184053043](https://www.pivotaltracker.com/story/show/184053043)

--- terraform/storage/provision/outputs.tf
+++ terraform/storage/provision/outputs.tf
@@ -1,2 +1,6 @@
 output "id" { value = google_storage_bucket.bucket.id }
 output "bucket_name" { value = var.name }
+output "status" { value = format("created bucket %s - URL: https://console.cloud.google.com/storage/browser/%s;tab=objects?project=%s",
+  google_storage_bucket.bucket.name,
+  google_storage_bucket.bucket.name, var.project
+) }
