56a326a278e6ef0943d47b0064ff9300dfd6bec8
Andrea Zucchini <zandrea@vmware.com>
2023-01-25 11:28:41 +0000

    feat: use the new MySQL Terraform Provider
    [#180913283](https://www.pivotaltracker.com/story/show/180913283)

--- terraform/cloudsql/mysql/bind/main.tf
+++ terraform/cloudsql/mysql/bind/main.tf
@@ -1,26 +1,4 @@
-resource "random_string" "username" {
-  length  = 16
-  special = false
-  numeric = false
-}
-
-resource "random_password" "password" {
-  length           = 64
-  override_special = "~_-."
-  min_upper        = 2
-  min_lower        = 2
-  min_special      = 2
-}
-
-resource "mysql_user" "newuser" {
-  user               = random_string.username.result
-  plaintext_password = random_password.password.result
-  host               = "%"
-}
-
-resource "mysql_grant" "newuser" {
-  user       = mysql_user.newuser.user
-  database   = var.mysql_db_name
-  host       = mysql_user.newuser.host
-  privileges = ["ALL"]
+resource "csbmysql_binding_user" "new_user" {
+  username = random_string.username.result
+  password = random_password.password.result
 }
--- terraform/cloudsql/mysql/bind/provider.tf
+++ terraform/cloudsql/mysql/bind/provider.tf
@@ -3,3 +3,15 @@ provider "mysql" {
   username = var.admin_username
   password = var.admin_password
 }
+
+provider "csbmysql" {
+  database    = var.mysql_db_name
+  password    = var.admin_password
+  username    = var.admin_username
+  port        = local.port
+  host        = var.private_ip
+  sslrootcert = var.sslrootcert
+  sslcert     = var.sslcert
+  sslkey      = var.sslkey
+  skip_verify = true
+}
