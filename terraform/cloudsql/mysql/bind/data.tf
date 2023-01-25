a76856b0ede6f8182200158831f0b30894316197
Andrea Zucchini <zandrea@vmware.com>
2023-01-25 16:41:03 +0000

    chore: resources belong in the main.tf; data is for local variables and calculations
    [#180913283](https://www.pivotaltracker.com/story/show/180913283)

--- terraform/cloudsql/mysql/bind/data.tf
+++ terraform/cloudsql/mysql/bind/data.tf
@@ -1,13 +1,3 @@
-resource "random_string" "username" {
-  length  = 16
-  special = false
-  numeric = false
-}
-
-resource "random_password" "password" {
-  length           = 64
-  override_special = "~_-."
-  min_upper        = 2
-  min_lower        = 2
-  min_special      = 2
+locals {
+  port = 3306
 }
