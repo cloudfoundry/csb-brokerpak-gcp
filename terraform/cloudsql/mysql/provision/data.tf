2639b529ca0104c1b682af110dcf0b14046e207f
Konstantin Semenov <ksemenov@pivotal.io>
2023-01-26 16:31:23 +0000

    fix: Enable backups block for transaction log backups
    [#184303419](https://www.pivotaltracker.com/story/show/184303419)

--- terraform/cloudsql/mysql/provision/data.tf
+++ terraform/cloudsql/mysql/provision/data.tf
@@ -4,8 +4,8 @@ data "google_compute_network" "authorized-network" {
 
 locals {
   authorized_network_id           = length(var.authorized_network_id) > 0 ? var.authorized_network_id : data.google_compute_network.authorized-network.self_link
-  backups_enabled                 = var.backups_retain_number > 0
   transaction_log_backups_enabled = var.backups_transaction_log_retention_days > 0
+  backups_enabled                 = var.backups_retain_number > 0 || local.transaction_log_backups_enabled
   availability_type               = var.highly_available ? "REGIONAL" : "ZONAL"
   primary_zone                    = var.location_preference_zone == "" ? "" : join("-", [var.region, var.location_preference_zone])
   secondary_zone                  = var.location_preference_secondary_zone == "" ? "" : join("-", [var.region, var.location_preference_secondary_zone])
