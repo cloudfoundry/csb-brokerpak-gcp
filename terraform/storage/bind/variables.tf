d92d18f50e59ab7e266d0fd990e14e940d5b81fb
Andrea Zucchini <zandrea@vmware.com>
2023-02-13 13:02:08 +0000

    chore: [Cloud Storage] add provider and versions template files (#673)
    Co-authored-by: George Blue <blgm@users.noreply.github.com>

--- terraform/storage/bind/variables.tf
+++ terraform/storage/bind/variables.tf
@@ -1,3 +1,5 @@
+variable "credentials" { type = string }
+variable "project" { type = string }
 variable "role" { type = string }
 variable "service_account_name" { type = string }
 variable "service_account_display_name" { type = string }
