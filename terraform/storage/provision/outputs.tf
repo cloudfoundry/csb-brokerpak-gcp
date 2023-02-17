e01cebd32c2d21309afffdffac91f7c84c0da459
Andrea Zucchini <zandrea@vmware.com>
2023-02-17 11:32:29 +0000

    chore: expose instance name as an output value (#672)
    * chore: add status property to MySQL outputs
    
    * chore: expose instance name as an output value
    
    ---------
    
    Co-authored-by: George Blue <blgm@users.noreply.github.com>

--- terraform/storage/provision/outputs.tf
+++ terraform/storage/provision/outputs.tf
@@ -1,6 +1,3 @@
 output "id" { value = google_storage_bucket.bucket.id }
 output "bucket_name" { value = var.name }
-output "status" { value = format("created bucket %s - URL: https://console.cloud.google.com/storage/browser/%[1]s;tab=objects?project=%s",
-  google_storage_bucket.bucket.name,
-  var.project
-) }
+output "status" { value = format("service %s created", google_storage_bucket.bucket.name) }
