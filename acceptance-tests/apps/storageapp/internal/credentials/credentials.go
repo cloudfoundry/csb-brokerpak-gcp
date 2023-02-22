d47e2f879bc4d9b25d47619b9e3f9a6d030270a8
Andrea Zucchini <zandrea@vmware.com>
2023-02-22 18:33:29 +0000

    test: [Cloud Storage] add migration test
    [#184053109](https://www.pivotaltracker.com/story/show/184053109)

--- acceptance-tests/apps/storageapp/internal/credentials/credentials.go
+++ acceptance-tests/apps/storageapp/internal/credentials/credentials.go
@@ -1,6 +1,7 @@
 package credentials
 
 import (
+	"encoding/base64"
 	"fmt"
 
 	"github.com/cloudfoundry-community/go-cfenv"
@@ -22,14 +23,46 @@ func Read() (StorageCredentials, error) {
 		return StorageCredentials{}, fmt.Errorf("error reading Storage service details")
 	}
 
+	switch svs[0].Label {
+	case "google-storage":
+		return readLegacyBrokerBinding(svs[0].Credentials)
+	default:
+		return readBinding(svs[0].Credentials)
+	}
+}
+
+func readBinding(creds any) (StorageCredentials, error) {
 	var r StorageCredentials
-	if err := mapstructure.Decode(svs[0].Credentials, &r); err != nil {
-		return StorageCredentials{}, fmt.Errorf("failed to decode credentials: %w", err)
+	if err := mapstructure.Decode(creds, &r); err != nil {
+		return r, fmt.Errorf("failed to decode credentials: %w", err)
 	}
 
 	if r.Credentials == "" || r.BucketName == "" {
-		return StorageCredentials{}, fmt.Errorf("parsed credentials are not valid")
+		return r, fmt.Errorf("parsed credentials are not valid")
 	}
 
+	return r, nil
+}
+
+func readLegacyBrokerBinding(creds any) (StorageCredentials, error) {
+	type legacyBindingData struct {
+		BucketName     string `mapstructure:"bucket_name"`
+		PrivateKeyData string `mapstructure:"PrivateKeyData"`
+	}
+	var (
+		r StorageCredentials
+		l legacyBindingData
+	)
+	if err := mapstructure.Decode(creds, &l); err != nil {
+		return r, err
+	}
+
+	v, err := base64.StdEncoding.DecodeString(l.PrivateKeyData)
+	if err != nil {
+		return r, fmt.Errorf("error decoding private key in JSON format, base64 encoded: %s", err.Error())
+	}
+
+	r.BucketName = l.BucketName
+	r.Credentials = string(v)
 	return r, nil
 }
