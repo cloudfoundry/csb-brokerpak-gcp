5ec6a475967b4f20bf2ef9ab609a86858c844462
Marcela Campo <mcampo@vmware.com>
2024-01-22 12:33:27 +0000

    chore: flag certs as sensitive in cloudsql terraform output (#1007)
    [#186876337](https://www.pivotaltracker.com/story/show/186876337)
    
    Co-authored-by: George Blue <blgm@users.noreply.github.com>

--- terraform/cloudsql/postgresql/provision/outputs.tf
+++ terraform/cloudsql/postgresql/provision/outputs.tf
@@ -9,10 +9,16 @@ output "password" {
 }
 output "require_ssl" { value = var.require_ssl }
 
-output "sslcert" { value = google_sql_ssl_cert.client_cert.cert }
+output "sslcert" {
+  sensitive = true
+  value     = google_sql_ssl_cert.client_cert.cert
+}
 output "sslkey" {
   value     = google_sql_ssl_cert.client_cert.private_key
   sensitive = true
 }
-output "sslrootcert" { value = google_sql_database_instance.instance.server_ca_cert.0.cert }
+output "sslrootcert" {
+  sensitive = true
+  value     = google_sql_database_instance.instance.server_ca_cert.0.cert
+}
 
