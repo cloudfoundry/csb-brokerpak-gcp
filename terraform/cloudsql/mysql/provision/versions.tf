1ded947e36bd17cfcecee3efc767a4f402eceb06
Felisia Martini <fmartini@pivotal.io>
2024-04-09 16:09:58 +0100

    chore: Define hostname for TF providers

--- terraform/cloudsql/mysql/provision/versions.tf
+++ terraform/cloudsql/mysql/provision/versions.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
+      source  = "registry.terraform.io/hashicorp/google"
       version = ">=4.8.0"
     }
     random = {
-      source  = "hashicorp/random"
+      source  = "registry.terraform.io/hashicorp/random"
       version = ">=3.4.3"
     }
   }
