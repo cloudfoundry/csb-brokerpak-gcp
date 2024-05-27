343dd19da4d38a60168a7d9130c4734e76a1edf5
Andrea Zucchini <zandrea@vmware.com>
2024-05-27 17:22:05 +0100

    chore: set old properties as deprecated
    [#187565525](https://www.pivotaltracker.com/story/show/187565525)

--- terraform/storage/bind/outputs.tf
+++ terraform/storage/bind/outputs.tf
@@ -6,6 +6,15 @@ output "PrivateKeyData" {
   value     = google_service_account_key.key.private_key
 }
 output "ProjectId" { value = google_service_account.account.project }
+output "private_key_data" {
+  sensitive   = true
+  value       = google_service_account_key.key.private_key
+  description = "Deprecated - The private key data of the service account"
+}
+output "project_id" {
+  value       = google_service_account.account.project
+  description = "Deprecated - The project ID of the service account"
+}
 output "credentials" {
   sensitive = true
   value     = base64decode(google_service_account_key.key.private_key)
