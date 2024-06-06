2c0d5e34dc34f47cd0b41887126a1ee6aa17a7db
Felisia Martini <fmartini@pivotal.io>
2024-06-06 14:35:03 +0100

    Add migration test from legacy broker
    This test validates that the app can continue to work without any changes if the binding changes from a legacy broker instance to a CSB instance

--- acceptance-tests/pubsub_test.go
+++ acceptance-tests/pubsub_test.go
@@ -40,6 +40,63 @@ var _ = Describe("PubSub", Label("pubsub"), func() {
 
 		By("retrieving a message with the subscriber app")
 		got := subscriberApp.GET("").String()
-		Expect(got).To(Equal(messageData))
+		Expect(got).To(Equal(messageData), "Received message matched published message")
+	})
+
+	When("using the legacy broker", func() {
+		It("can continue using the same app with CSB service instance", func() {
+			By("creating a legacy service instance")
+			legacySubscription := random.Name()
+			legacyInstance := services.CreateInstance(
+				"google-pubsub",
+				"default",
+				services.WithBrokerName("legacy-gcp-broker"),
+				services.WithParameters(map[string]any{"subscription_name": legacySubscription}))
+			defer legacyInstance.Delete()
+
+			By("pushing the unstarted app twice")
+			publisherApp := apps.Push(apps.WithApp(apps.PubSubApp))
+			subscriberApp := apps.Push(apps.WithApp(apps.PubSubApp))
+			defer apps.Delete(publisherApp, subscriberApp)
+
+			By("binding the apps to the storage service instance")
+			legacyPubBinding := legacyInstance.BindWithParams(publisherApp, `{"role":"pubsub.editor"}`)
+			legacySubBinding := legacyInstance.BindWithParams(subscriberApp, `{"role":"pubsub.editor"}`)
+
+			By("starting the apps")
+			apps.Start(publisherApp, subscriberApp)
+
+			By("publishing a message with the publisher app")
+			messageData := random.Hexadecimal()
+			publisherApp.PUT(messageData, "")
+
+			By("retrieving a message with the subscriber app")
+			got := subscriberApp.GET("").String()
+			Expect(got).To(Equal(messageData), "Received message matched published message")
+
+			By("creating a CSB service instance")
+			CSBServiceInstance := services.CreateInstance(
+				"csb-google-pubsub",
+				"default",
+				services.WithParameters(map[string]any{"subscription_name": random.Name()}))
+			defer CSBServiceInstance.Delete()
+
+			By("unbinding the apps from legacy service instance and binding them to CSB instance")
+			legacyPubBinding.Unbind()
+			legacySubBinding.Unbind()
+			CSBServiceInstance.BindWithParams(publisherApp, `{"role":"pubsub.editor"}`)
+			CSBServiceInstance.BindWithParams(subscriberApp, `{"role":"pubsub.editor"}`)
+
+			By("starting the apps")
+			apps.Restage(publisherApp, subscriberApp)
+
+			By("publishing a message with the publisher app")
+			newMessageData := random.Hexadecimal()
+			publisherApp.PUT(newMessageData, "")
+
+			By("retrieving a message with the subscriber app")
+			result := subscriberApp.GET("").String()
+			Expect(result).To(Equal(newMessageData), "Received message matched published message")
+		})
 	})
 })
