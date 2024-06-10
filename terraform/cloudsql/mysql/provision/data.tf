e7ef0e71e006778184ca11614907bb8e55a9a2cc
Felisia Martini <fmartini@pivotal.io>
2024-06-10 15:45:00 +0100

    fix: Should check for authorised network only when it being used (#1213)
    When `authorized_network_id` we do not need to check for the existence of `var.authorized_network`
    
    [#187737556](https://www.pivotaltracker.com/story/show/183975388)

--- terraform/cloudsql/mysql/provision/data.tf
+++ terraform/cloudsql/mysql/provision/data.tf
@@ -1,9 +1,10 @@
 data "google_compute_network" "authorized-network" {
-  name = "default"
+  count = length(var.authorized_network_id) > 0 ? 0 : 1
+  name  = "default"
 }
 
 locals {
-  authorized_network_id           = length(var.authorized_network_id) > 0 ? var.authorized_network_id : data.google_compute_network.authorized-network.self_link
+  authorized_network_id           = length(var.authorized_network_id) > 0 ? var.authorized_network_id : data.google_compute_network.authorized-network[0].self_link
   transaction_log_backups_enabled = var.backups_transaction_log_retention_days > 0
   backups_enabled                 = var.backups_retain_number > 0 || local.transaction_log_backups_enabled
   availability_type               = var.highly_available ? "REGIONAL" : "ZONAL"
--- terraform/redis/provision/data.tf
+++ terraform/redis/provision/data.tf
@@ -1,7 +1,8 @@
 data "google_compute_network" "authorized-network" {
-  name = var.authorized_network
+  count = length(var.authorized_network_id) > 0 ? 0 : 1
+  name  = var.authorized_network
 }
 
 locals {
-  authorized_network_id = length(var.authorized_network_id) > 0 ? var.authorized_network_id : data.google_compute_network.authorized-network.self_link
+  authorized_network_id = length(var.authorized_network_id) > 0 ? var.authorized_network_id : data.google_compute_network.authorized-network[0].self_link
 }
