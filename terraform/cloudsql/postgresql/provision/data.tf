f0eb51c977716aa423b62d141e7874f1640328cf
Felisia Martini <fmartini@pivotal.io>
2024-06-10 14:23:48 +0100

    fix: Should check for authorised network only when it being used
    When `authorized_network_id` we do not need to check for the existence of `var.authorized_network`
    
    [#187737556](https://www.pivotaltracker.com/story/show/183975388)

--- terraform/cloudsql/postgresql/provision/data.tf
+++ terraform/cloudsql/postgresql/provision/data.tf
@@ -1,9 +1,10 @@
 data "google_compute_network" "authorized-network" {
-  name = var.authorized_network
+  count = length(var.authorized_network_id) > 0 ? 0 : 1
+  name  = var.authorized_network
 }
 
 locals {
-  authorized_network_id = length(var.authorized_network_id) > 0 ? var.authorized_network_id : data.google_compute_network.authorized-network.self_link
+  authorized_network_id = length(var.authorized_network_id) > 0 ? var.authorized_network_id : data.google_compute_network.authorized-network[0].self_link
   availability_type     = var.highly_available ? "REGIONAL" : "ZONAL"
   primary_zone          = var.location_preference_zone == "" ? "" : join("-", [var.region, var.location_preference_zone])
   secondary_zone        = var.location_preference_secondary_zone == "" ? "" : join("-", [var.region, var.location_preference_secondary_zone])
