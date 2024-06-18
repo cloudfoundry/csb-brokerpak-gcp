b55a21efc8fac371026b84bea5699e5f078ca13c
Andrea Zucchini <zandrea@vmware.com>
2024-06-18 14:33:07 +0100

    chore: keep same ordering as YML definition
    [#187726204](https://www.pivotaltracker.com/story/show/187726204)

--- terraform/pubsub/provision/variables.tf
+++ terraform/pubsub/provision/variables.tf
@@ -2,13 +2,6 @@ variable "topic_name" { type = string }
 variable "subscription_name" { type = string }
 variable "ack_deadline" { type = number }
 variable "push_endpoint" { type = string }
-variable "labels" { type = map(any) }
-variable "credentials" {
-  type      = string
-  sensitive = true
-}
-variable "project" { type = string }
-
 variable "topic_message_retention_duration" { type = string }
 variable "topic_kms_key_name" { type = string }
 variable "subscription_message_retention_duration" { type = string }
@@ -18,3 +11,9 @@ variable "subscription_retry_policy_minimum_backoff" { type = string }
 variable "subscription_retry_policy_maximum_backoff" { type = string }
 variable "subscription_enable_message_ordering" { type = bool }
 variable "subscription_enable_exactly_once_delivery" { type = bool }
+variable "labels" { type = map(any) }
+variable "credentials" {
+  type      = string
+  sensitive = true
+}
+variable "project" { type = string }
