b8a4c84d00b5c4e0ee702bbb27d238e53db31ded
Andrea Zucchini <zandrea@vmware.com>
2024-08-30 11:21:07 +0100

    feat!: Retirement GCP Dataproc offering

--- terraform/dataproc/bind/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "credentials" {
-  type      = string
-  sensitive = true
-}
-variable "project" { type = string }
-variable "service_account_name" { type = string }
-variable "bucket" { type = string }
--- terraform/dataproc/provision/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "bucket_name" { value = google_dataproc_cluster.cluster.cluster_config.0.bucket }
-output "cluster_name" { value = google_dataproc_cluster.cluster.name }
-output "region" { value = google_dataproc_cluster.cluster.region }
