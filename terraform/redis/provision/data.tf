f4b009162a1f7432c2b3a50cab4d33e5e2977bc1
ifindlay-cci <fiain@vmware.com>
2024-09-19 16:39:57 +0100

    feat!: Retirement of GCP Redis offering
    [#TPCF-27143](https://vmw-jira.broadcom.com/browse/TPCF-27143)

--- terraform/redis/provision/data.tf
+++
@@ -1,8 +0,0 @@
-data "google_compute_network" "authorized-network" {
-  count = length(var.authorized_network_id) > 0 ? 0 : 1
-  name  = var.authorized_network
-}
-
-locals {
-  authorized_network_id = length(var.authorized_network_id) > 0 ? var.authorized_network_id : data.google_compute_network.authorized-network[0].self_link
-}
