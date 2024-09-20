b43da7cc67d803c9e6ac2c373e31fa50c876cddf
ifindlay-cci <84311346+ifindlay-cci@users.noreply.github.com>
2024-09-20 13:09:30 +0100

    feat!: Removed bigquery offering (#1348)
    [#TPCF-27145](https://vmw-jira.broadcom.com/browse/TPCF-27145)

--- terraform/bigquery/bind/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "dataset_id" { type = string }
-variable "role" { type = string }
-variable "service_account_name" { type = string }
-variable "service_account_display_name" { type = string }
-variable "project" { type = string }
-variable "credentials" {
-  type      = string
-  sensitive = true
-}
--- terraform/bigquery/provision/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "credentials" {
-  type      = string
-  sensitive = true
-}
-variable "project" { type = string }
-variable "labels" { type = map(any) }
-variable "region" { type = string }
-variable "instance_name" { type = string }
