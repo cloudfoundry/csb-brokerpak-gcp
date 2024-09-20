5e2f0a0b4c8016f7c9749168210d5e2dcf001489
ifindlay-cci <84311346+ifindlay-cci@users.noreply.github.com>
2024-09-20 09:33:53 +0100

    feat!: Retirement of GCP Redis offering (#1342)
    [#TPCF-27143](https://vmw-jira.broadcom.com/browse/TPCF-27143)
    
    Co-authored-by: Felisia Martini <fmartini@pivotal.io>

--- terraform/redis/provision/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "memory_size_gb" { value = google_redis_instance.instance.memory_size_gb }
-output "service_tier" { value = google_redis_instance.instance.tier }
-output "redis_version" { value = google_redis_instance.instance.redis_version }
-output "host" { value = google_redis_instance.instance.host }
-output "port" { value = google_redis_instance.instance.port }
