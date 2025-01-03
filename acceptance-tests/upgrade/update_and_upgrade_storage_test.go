9be0bbb4837cb13bf84ba90a11b2a4733a78b776
ifindlay-cci <84311346+ifindlay-cci@users.noreply.github.com>
2025-01-03 11:28:19 +0000

    chore: Fixes broker 'failed to allocate' test errors (#1420)
    We can only have 5 instances of our test broker running. Tests that use the test broker can see 'failed to allocate' errors.
    This is because in the case of a failed creation of a service in our tests will fail to clean up the test broker, causing other test runs to fail.

--- acceptance-tests/upgrade/update_and_upgrade_storage_test.go
+++ acceptance-tests/upgrade/update_and_upgrade_storage_test.go
@@ -23,12 +23,20 @@ var _ = Describe("UpgradeStorageTest", Label("storage"), func() {
 			defer serviceBroker.Delete()
 
 			By("creating a service")
+			serviceOffering := "csb-google-storage-bucket"
+			servicePlan := "default"
+			serviceName := random.Name(random.WithPrefix(serviceOffering, servicePlan))
+			// CreateInstance can fail and can leave a service record (albeit a failed one) lying around.
+			// We can't delete service brokers that have serviceInstances, so we need to ensure the service instance
+			// is cleaned up regardless as to whether it wa successful. This is important when we use our own service broker
+			// (which can only have 5 instances at any time) to prevent subsequent test failures.
+			defer services.Delete(serviceName)
 			serviceInstance := services.CreateInstance(
-				"csb-google-storage-bucket",
-				"default",
+				serviceOffering,
+				servicePlan,
 				services.WithBroker(serviceBroker),
+				services.WithName(serviceName),
 			)
-			defer serviceInstance.Delete()
 
 			By("pushing the unstarted app twice")
 			appOne := apps.Push(apps.WithApp(apps.Storage))
@@ -53,7 +61,7 @@ var _ = Describe("UpgradeStorageTest", Label("storage"), func() {
 			serviceBroker.UpdateBroker(developmentBuildDir)
 
 			By("validating that the instance plan is still active")
-			Expect(plans.ExistsAndAvailable("default", "csb-google-storage-bucket", serviceBroker.Name))
+			Expect(plans.ExistsAndAvailable(servicePlan, serviceOffering, serviceBroker.Name))
 
 			By("upgrading service instance")
 			serviceInstance.Upgrade()
