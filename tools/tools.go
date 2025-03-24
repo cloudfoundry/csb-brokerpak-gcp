5bf75de4a1c9251e6430f5b27a516334c08c757a
George Blue <blgm@users.noreply.github.com>
2025-03-24 15:20:28 +0000

    chore: use Go 1.24 tools management (#1443)

--- tools/tools.go
+++ tools/tools.go
@@ -1,13 +1,11 @@
 //go:build tools
+// +build tools
 
 package tools
 
 import (
 	_ "github.com/cloudfoundry/cloud-service-broker/v2"
-	_ "github.com/onsi/ginkgo/v2/ginkgo"
-	_ "golang.org/x/tools/cmd/goimports"
-	_ "honnef.co/go/tools/cmd/staticcheck"
 )
 
-// This file imports packages that are used when running go generate, or used
-// during the development process but not otherwise depended on by built code.
+// This file imports the Cloud Service Broker in order to pin the version
+// For other tools, use "go get -tool"
